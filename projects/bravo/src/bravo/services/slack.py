4069d79e3fb1cb91b88ddb113ec0c644966e402b
blueman82 <harrison@adobe.com>
2026-02-16 17:31:22 +0000

    Update Python: slack.py via Edit [2026-02-16 17:31]

--- projects/bravo/src/bravo/services/slack.py
+++ projects/bravo/src/bravo/services/slack.py
@@ -23,9 +23,11 @@ from bravo.protocols import JiraClientProto, PATServiceProto
 from bravo.services.blocks import (
     build_acknowledged_blocks,
     build_collect_pat_modal,
+    build_comment_modal,
     build_fix_error_blocks,
     build_fix_now_modal,
     build_fix_submitted_blocks,
+    build_pat_error_modal,
     build_snoozed_blocks,
     build_unsnoozed_blocks,
     build_yes_updates_blocks,
