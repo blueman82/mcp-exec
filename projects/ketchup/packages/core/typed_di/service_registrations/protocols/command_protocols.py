e7197143423bdd82d44eff5a4ed1d4017e027c8f
blueman82 <harrison@adobe.com>
2026-03-30 14:27:39 +0100

    Update Python: command_protocols.py via Edit [2026-03-30 14:27]

--- projects/ketchup/packages/core/typed_di/service_registrations/protocols/command_protocols.py
+++ projects/ketchup/packages/core/typed_di/service_registrations/protocols/command_protocols.py
@@ -12,7 +12,6 @@ __all__ = [
     "QueryCommandProtocol",
     "VerifyCommandProtocol",
     "StatusReportCommandProtocol",
-    "ShortLongCommandProtocol",
     "SlackArchiveCommandProtocol",
     "SlackListCommandProtocol",
     "SlackQueryHandlerProtocol",
